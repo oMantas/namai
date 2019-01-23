0969a924f3a38c1398b6b2364e0132642c98c312
Mantas <mantas.aaaa@gmail.com>
2019-01-23 15:57:09 +0200

    7uzd

--- index.php
+++ index.php
@@ -1,46 +1,28 @@
 <?php
-date_default_timezone_get('Europe/Vilnius');
-$time = date('s');
-$id = '';
+$days = 365;
+$cig_cost = 3.60 / 20;
+$cig_per_year = 0;
+$dontSmoke_monThu = 0;
 
-if($time % 2 == 0) {
-    $id = 'kvadratas';
-}else {
-    $id = 'apskritimas';
-}
+for($i = 0; $i < $days; $i++) {
+    $week_day = date('N', strtotime("+$i day"));
+    if($week_day < 5) {
+        $cig_per_monThu = rand(1,5);
+        $cig_per_year += $cig_per_monThu;
+        $dontSmoke_monThu += $cig_per_monThu;
+    }elseif($week_day == 5) {
+        $cig_per_fri = rand(15,25);
+        $cig_per_year += $cig_per_fri;
+    }else {
+        $cig_per_satSun = rand(8,12);
+        $cig_per_year += $cig_per_satSun;
+    }
+};
+
+$isleisti_eur = $cig_per_year * $cig_cost;
+$save_from_dontSmoke = $dontSmoke_monThu * $cig_cost;
+
+print "Per metus surukysiu $cig_per_year cigareciu, uz $isleisti_eur eur. <br>";
+print "Nerukydamas nuo pirmadienio iki ketvirtadienio sutaupyciau $save_from_dontSmoke eur."
+
 ?>
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Document</title>
-        <style>
-            p {
-                color: white;
-                font-family: fantasy;
-                font-size: 40px;
-                text-align: center;
-                padding-top: 165px;
-            }
-            div {
-                width: 400px;
-                height: 400px;
-                margin: 150px auto;
-            }
-            #kvadratas {
-                background-color: black;
-            }
-            #apskritimas {
-                border-radius: 50%;
-                background-color: grey;
-            }
-        </style>
-    </head>
-    <body>
-        <div id='<?php print "$id"; ?>'>
-            <p><?php print date('H:i:s'); ?></p>
-        </div>
-    </body>
-</html>
